524d8efeb872d41ff0a8bc1dacc5e66a52357563
yuvraj024 <oberoiyuvraj024@gmail.com>
2024-03-14 11:13:46 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -14,7 +14,14 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 // url me jo encoding aajati h usko accept krane kelie
 app.use(express.static("public"))
 // server me temp storage isme krado images and all store
-
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+// http://localhost:8000/api/v1/users/register
 
 export {app}
